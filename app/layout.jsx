2d0e49098bc6aaf4bc0bbdc1d5d9938f07c6638e
ericpangaiii <evpanga2@up.edu.ph>
2024-06-29 20:13:27 +0800

    feat: added ThemeProvider

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,11 +14,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
+        <ThemeProvider attribute = "class" defaultTheme = 'light'>
+          <Header/>
+          {children}
+          <Footer/>
+        </ThemeProvider>
         </body>
     </html>
   );
